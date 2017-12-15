5af8532be8f147c9d12e40caab90ddcc90a3dcc5
Eric Solner <Eric@Erics-MacBook-Pro.local>
2017-12-14 22:04:56 -0600

    fixed title in index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,82 +1,83 @@
 $(document).ready(function() {
 
-	crystals = ['assets/images/coin.png','assets/images/coin.png','assets/images/coin.png','assets/images/coin.png'];
+    crystals = ['assets/images/coin.png', 'assets/images/coin.png', 'assets/images/coin.png', 'assets/images/coin.png'];
 
-	var counter = 0;
-	var wins = 0;
-	var losses = 0;
-	$('#win').text(wins);
-	$('#loss').text(losses);
-	
-	newCrystals();
-	newGame();
+    var counter = 0;
+    var wins = 0;
+    var losses = 0;
+    $('#win').text(wins);
+    $('#loss').text(losses);
 
-	function newCrystals () {
-		var numbers = []
-			while(numbers.length < 4){
-			  var randomnumber = Math.ceil(Math.random()*12)
-			  var found = false;
-			  for (var i=0; i< numbers.length; i++){
-				if (numbers[i] == randomnumber){
-					found = true; break
-				}
-			  }
-			  if(!found)numbers[numbers.length]=randomnumber;
-			}
-		console.log(numbers);		
+    newCrystals();
+    newGame();
 
-		for (i = 0; i < numbers.length; i++) {
-			var imageCrystal = $('<img>');
-			imageCrystal.attr('data-num', numbers[i]);
-			imageCrystal.attr('src', crystals[i]);
-			imageCrystal.attr('alt', 'crystals');
-			imageCrystal.addClass('crystalImage')
-			$('#crystals').append(imageCrystal);
-		}
-	}
+    function newCrystals() {
+        var numbers = []
+        while (numbers.length < 4) {
+            var randomnumber = Math.ceil(Math.random() * 12)
+            var found = false;
+            for (var i = 0; i < numbers.length; i++) {
+                if (numbers[i] == randomnumber) {
+                    found = true;
+                    break
+                }
+            }
+            if (!found) numbers[numbers.length] = randomnumber;
+        }
+        
 
-	function newGame() {
+        for (i = 0; i < numbers.length; i++) {
+            var imageCrystal = $('<img>');
+            imageCrystal.attr('data-num', numbers[i]);
+            imageCrystal.attr('src', crystals[i]);
+            imageCrystal.attr('alt', 'crystals');
+            imageCrystal.addClass('crystalImage')
+            $('#crystals').append(imageCrystal);
+        }
+    }
 
-		counter = 0;
-		$('#yourScore').text(counter);
+    function newGame() {
 
-		function randomIntFromInterval(min,max){
-		   	return Math.floor(Math.random()*(max-min+1)+min);
-			}
+        counter = 0;
+        $('#yourScore').text(counter);
 
-		var numberToGuess = randomIntFromInterval(19,120);
+        function randomIntFromInterval(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
 
-		$('.value').text(numberToGuess);
+        var numberToGuess = randomIntFromInterval(19, 120);
+
+        $('.value').text(numberToGuess);
 
 
-		$('.crystalImage').on('click', function(){
-		    counter = counter + parseInt($(this).data('num'));
-		   
-		    $('#yourScore').text(counter);
+        $('.crystalImage').on('click', function() {
+            counter = counter + parseInt($(this).data('num'));
 
-		    if (counter == numberToGuess){
-		      $('#status').text('You won!');
-		      wins ++;
-		      $('#win').text(wins);
-		      console.log(wins)
-		      $('#crystals').empty();
-		      newCrystals();
-		      newGame();
-		        
-		    } else if ( counter > numberToGuess){
-		        $('#status').text('You lost!')
-		        losses ++;
-		        $('#loss').text(losses);
-		        console.log(losses)
-		        $('#crystals').empty();
-		        newCrystals();
-		        newGame();
-		    }
+            $('#yourScore').text(counter);
 
-		    if (wins == 5) {
-		    	alert("You Found One Eyed Willies Treasure!");
-		    }
-		});
-	}
+            if (counter == numberToGuess) {
+                $('#status').text('You won!');
+                wins++;
+                $('#win').text(wins);
+                console.log(wins)
+                $('#crystals').empty();
+                newCrystals();
+                newGame();
+
+            } else if (counter > numberToGuess) {
+                $('#status').text('You lost!')
+                losses++;
+                $('#loss').text(losses);
+                console.log(losses)
+                $('#crystals').empty();
+                newCrystals();
+                newGame();
+            }
+
+            if (wins == 5) {
+                alert("You Found One Eyed Willies Treasure!");
+            }
+        });
+    }
 
 });
